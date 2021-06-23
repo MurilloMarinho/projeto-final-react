787555751958897e4cc0e5cf519f2db5d0b4291d
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-06-23 16:10:11 -0300

    .

--- src/Paginas/SignIn/index.jsx
+++ src/Paginas/SignIn/index.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-<<<<<<< HEAD
 import http from '../../http'
-=======
-import http from "../../http/index"
->>>>>>> 3d4477d86627dbc757589254fa839b70b2ec65e2
 const SignIn = () => {
 
     const [nome, setNome] = useState('')
