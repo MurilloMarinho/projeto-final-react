5dcdb3b995d5e23514b935aa708843f21dcadb90
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-06-23 10:57:58 -0300

    .

--- backend Projeto Final/trabalho_final_API_restfull-main/Loja/src/main/java/org/serratec/repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.serratec.repository;
-
-import org.serratec.model.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-
-}
