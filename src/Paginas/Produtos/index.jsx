c74b79b3679526657705a49dd3594cf27401fac2
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-06-23 13:35:02 -0300

    .

--- src/Paginas/Produtos/index.jsx
+++ src/Paginas/Produtos/index.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react"
+import http from '../../http'
+
 const Produtos = () => {
+    const [produtos, setProdutos] = useState([])
+
+    useEffect(() => {
+        http.get('itens').then((response) => setProdutos(response)).catch(erro => console.log(erro))
+    }, [])
+
     return (
-        <div></div>
+        <div>
+            {produtos.map((item) =>
+                <ProdutoCard key={item.id} nome={item.nome} preco={item.preco} />
+            )}
+        </div>
     )
 }
 
